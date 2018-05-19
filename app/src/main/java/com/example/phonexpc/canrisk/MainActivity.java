7c17915f853e526b3636da3446339eafff340c5f
James Kamana <jaykamana@gmail.com>
2018-05-19 14:01:43 +0300

    Add the first set of questions.

--- app/src/main/java/com/example/phonexpc/canrisk/MainActivity.java
+++ app/src/main/java/com/example/phonexpc/canrisk/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.phonexpc.canrisk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btnStartTest);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Page1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
